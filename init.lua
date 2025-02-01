0b497623680a57995547d6e0368f38f82c50216d
Murali I. <indukurimkv@gmail.com>
2025-01-31 23:58:39 -0500

    update init.lua

--- init.lua
+++ init.lua
@@ -164,6 +164,8 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+vim.keymap.set('n', 'q', '<C-v>')
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
