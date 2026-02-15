44afc5bce95c44681c8bb1d338030b4a3bdd2b1a
Murali Indukuri <indukurimkv@gmail.com>
2026-02-14 23:50:39 -0500

    Modify init

--- init.lua
+++ init.lua
@@ -91,6 +91,8 @@ end
 
 -- Set shiftwidth
 vim.opt.shiftwidth = 4
+-- Expand tabs to spaces
+vim.opt.expandtab = true
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
